aee03fe2f934be524a06ada32e7e6611c3d12d8e
Aimé Ngouinda <juniorngouinda@gmail.com>
2025-05-13 14:52:18 +0200

    FEATURE: Dockerisation du projet WIP

--- frontservice/src/App.jsx
+++ frontservice/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Categories from './components/Categorie/Categorie'
 import Depenses from './components/Depenses/Depenses'
+import Home from './components/Home/Home'
 
 
 function App() {
@@ -8,8 +10,13 @@ function App() {
 
   return (
     <>
-       <Depenses/>
-       <Categories/>
+       <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/depenses" element={<Depenses/>} />
+            <Route path="/categories" element={<Categories/>} />
+          </Routes>
+       </BrowserRouter>
     </>
   )
 }
---
+++ frontservice/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+             Homme page
+        </div>
+    );
+};
+
+export default Home;
